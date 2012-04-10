925ca6f95e6fd12124b4a71a22841ec240924e1c
ronalfei <ronalfei@qq.com>
2012-04-10 16:13:09 +0800

    del comment

--- webroot/router.php
+++ webroot/router.php
@@ -9,15 +9,5 @@ try{
 	echo $e->getMessage().debug_print_backtrace();
 	echo "<br/>\r\n----------------------------------------<br/>\r\n";
 }
-//$class		= $codeAnt->input->get('c');
-//$function	= $codeAnt->input->get('f');
-//$args		= $codeAnt->input->get('a');
-//
-//
-//$params		= explode($args,'/');
-//
-//$object		= new $class();
-//call_user_func_array($object, $function, $params);
-//
 
 ?>
