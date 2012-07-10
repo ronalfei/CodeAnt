0c3879ec304baee43b24c54c8f7e78cbdc37233b
ronalfei <ronalfei@qq.com>
2012-07-10 13:40:03 +0800

    add response method

--- lib/core/codeant/codeant.class.php
+++ lib/core/codeant/codeant.class.php
@@ -70,6 +70,14 @@ class codeant
 		$this->tpl->display($tpl);
 		$this->debug();
 	}
+	public function response($data, $status)
+	{
+		$response['result'] = $data;
+		$response['status'] = $status;
+		$response_json = json_encode($response);
+		header('Content-Type:application/json; Charset=utf-8');
+		echo $response_json;
+	}
 	private function setController($controller)
 	{
 		$this->controller = $controller;
