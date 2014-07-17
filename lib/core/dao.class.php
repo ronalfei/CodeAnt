eb158b30bb0be27e84957a26060d1a1c2be42e5e
ronalfei <ronalfei@qq.com>
2014-07-17 11:21:14 +0800

    fix bug

--- lib/core/dao.class.php
+++ lib/core/dao.class.php
@@ -1,41 +1,48 @@
 <?php
 class dao
 {
-	private $codeAnt;
-	
-	public function __construct()
-	{
-		$this->setCodeAnt();
-	}
+    protected $codeAnt;
+    protected $db;
 
-	private function setCodeAnt()
-	{
-		global $codeAnt;
-		$this->codeAnt = &$codeAnt;
-	}
+    public function __construct()
+    {
+        $this->setCodeAnt();
+    }
 
-	public static function codeAnt()
-	{
-		global $codeAnt;
-		return $codeAnt;
-	}
+    private function setCodeAnt()
+    {
+        global $codeAnt;
+        $this->codeAnt = &$codeAnt;
+    }
 
-	public function __get($var_name)
-	{
-		if(strtolower($var_name)=='codeant'){
-			global $codeAnt;
-			$autoGet = &$codeAnt;
-			return $autoGet; 
-		}else{
-			$var_name = addslashes($var_name);
-			throw new exception("你访问的变量{$var_name}不存在");
-		}
-		
-	}
-	public static function __callStatic($function_name, $params)
-	{
-		//nothing todo;
-	}
+    public static function codeAnt()
+    {
+        global $codeAnt;
+        return $codeAnt;
+    }
+
+    public static function db()
+    {
+        $ca = self::codeAnt();
+        return $ca->db;
+    }
+
+    public function __get($var_name)
+    {
+        if(strtolower($var_name)=='codeant'){
+            global $codeAnt;
+            $autoGet = &$codeAnt;
+            return $autoGet;
+        }else{
+            $var_name = addslashes($var_name);
+            throw new exception("你访问的变量{$var_name}不存在");
+        }
+
+    }
+    public static function __callStatic($function_name, $params)
+    {
+        //nothing todo;
+    }
 }
 
 
