fccaabdd98eacd5197f2f936afc77f47e6a1a80d
ronalfei <ronalfei@qq.com>
2014-08-06 16:10:29 +0800

    easy to use db instance

--- lib/core/controller.class.php
+++ lib/core/controller.class.php
@@ -2,6 +2,7 @@
 class controller 
 {
 	private $codeAnt;
+	public $db;
 	
 	public function __construct()
 	{
@@ -12,6 +13,7 @@ class controller
 	{
 		global $codeAnt;
 		$this->codeAnt = &$codeAnt;
+		$this->db = $this->codeAnt->db;
 	}
 
 	public static function codeAnt()
@@ -22,14 +24,19 @@ class controller
 
 	public function __get($var_name)
 	{
-		if(strtolower($var_name)=='codeant'){
-			global $codeAnt;
-			$autoGet = &$codeAnt;
-			return $autoGet; 
-		}else{
-			$var_name = addslashes($var_name);
-			throw new cexception("你访问的变量{$var_name}不存在");
-		}
+	    global $codeAnt;
+        $var_name = strtolower($var_name)
+        switch($var_name){
+            case 'codeant':
+			    $autoGet = &$codeAnt;
+			    return $autoGet; 
+            break;
+
+            default:
+			    $var_name = addslashes($var_name);
+			    throw new cexception("你访问的变量{$var_name}不存在");
+            break;
+        }
 		
 	}
 }
