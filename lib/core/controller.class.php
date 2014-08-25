7c04088b0326f05e8c4829d2617803fc88247d06
ronalfei <ronalfei@qq.com>
2014-08-25 18:47:47 +0800

    add usual object to __get

--- lib/core/controller.class.php
+++ lib/core/controller.class.php
@@ -23,22 +23,37 @@ class controller
 	}
 
 	public function __get($var_name)
-	{
-	    global $codeAnt;
+    {
+        global $codeAnt;
         $var_name = strtolower($var_name);
         switch($var_name){
             case 'codeant':
-			    $autoGet = &$codeAnt;
-			    return $autoGet; 
+                $autoGet = &$codeAnt;
+                return $autoGet;
+            break;
+
+            case 'db':
+                $autoGet = &$codeAnt;
+                return $this->codeAnt->db;
+            break;
+
+            case 'input':
+                $autoGet = &$codeAnt;
+                return $this->codeAnt->input;
+            break;
+
+            case 'tpl':
+                $autoGet = &$codeAnt;
+                return $this->codeAnt->tpl;
             break;
 
             default:
-			    $var_name = addslashes($var_name);
-			    throw new cexception("你访问的变量{$var_name}不存在");
+                $var_name = addslashes($var_name);
+                throw new cexception("你访问的变量{$var_name}不存在");
             break;
         }
-		
-	}
+
+    }
 }
 
 
